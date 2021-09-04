248d1b77d55a152873f26bbd5e6cc03010228e32
Brian Yarr <brian.yarr@deliveroo.co.uk>
2021-09-04 19:20:44 +0100

    Handle micro and nano - with tests!

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 use std::env::args_os;
 use std::process::exit;
-use std::convert::{TryFrom, Infallible, TryInto};
 use chrono::{Utc, TimeZone, DateTime, Local};
-use std::error::Error;
 
 fn main() {
     let args: Vec<_> = args_os().collect();
@@ -25,9 +23,6 @@ fn main() {
 
         }
     }
-
-
-
 }
 
 fn try_parse(input: String) -> Option<DateTime<Utc>> {
@@ -40,13 +35,50 @@ fn try_parse(input: String) -> Option<DateTime<Utc>> {
 }
 
 fn int_to_datetime(i: i64) -> DateTime<Utc> {
-    if i < 10000000000 {
+    if i < 10_000_000_000 {
         Utc.timestamp(i, 0)
-    } else {
+    } else if i < 10_000_000_000_000 {
         //assume epoch millis
-        let seconds = i / 1000;
-        let millis = (i - seconds * 1000);
-        let nanos = millis * 1000000;
+        let seconds = i / 1_000;
+        let millis = i - seconds * 1000;
+        let nanos = millis * 1_000_000;
         Utc.timestamp(seconds, nanos as u32)
+    } else if i < 10_000_000_000_000_000 {
+        // micro
+        let seconds = i / 1_000_000;
+        let micros = i - seconds * 1_000_000;
+        let nanos = micros * 1_000;
+        Utc.timestamp(seconds, nanos as u32)
+    } else {
+        //nano
+        let seconds = i / 1_000_000_000;
+        let nanos = i - seconds * 1_000_000_000;
+        Utc.timestamp(seconds, nanos as u32)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_int_to_datetime_seconds() {
+        assert_eq!(int_to_datetime(1630779114), Utc.ymd(2021, 9, 4).and_hms(18, 11, 54));
+    }
+
+    #[test]
+    fn test_int_to_datetime_milliseconds() {
+        assert_eq!(int_to_datetime(1630779114123), Utc.ymd(2021, 9, 4).and_hms_milli(18, 11, 54, 123));
+    }
+
+    #[test]
+    fn test_int_to_datetime_microseconds() {
+        assert_eq!(int_to_datetime(1630779114123456), Utc.ymd(2021, 9, 4).and_hms_micro(18, 11, 54, 123456));
+    }
+
+    #[test]
+    fn test_int_to_datetime_nanoseconds() {
+        assert_eq!(int_to_datetime(1630779114123456789), Utc.ymd(2021, 9, 4).and_hms_nano(18, 11, 54, 123456789));
     }
 }
